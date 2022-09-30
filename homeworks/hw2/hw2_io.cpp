6faa9653c6e8c165e374d6a4a63333cb5d0460e1
[emily sonant] <[emilysonant@gmail.com]>
2022-09-30 00:20:35 -0400

    commit for the date of birth

--- homeworks/hw2/hw2_io.cpp
+++ homeworks/hw2/hw2_io.cpp
@@ -5,4 +5,10 @@ int main() {
     std::cout << "Please enter your title, first name, and last name";
     std::string title, fname, lname;
     std::cin >> title >> fname >> lname;
+
+    //asks for dob
+    std::cout << "Please enter your date of birth mm/dd/yyyy";
+    std::string dob;
+    std::cin >> dob;
+    
 }
