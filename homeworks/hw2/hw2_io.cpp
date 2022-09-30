531a2ed01927e6381ca79fac3393ef3e496e550c
[emily sonant] <[emilysonant@gmail.com]>
2022-09-29 23:58:43 -0400

    commit for the title and name git status

---
+++ homeworks/hw2/hw2_io.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    //asks for title, first name, and last name
+    std::cout << "Please enter your title, first name, and last name";
+    std::string title, fname, lname;
+    std::cin >> title >> fname >> lname;
+}
