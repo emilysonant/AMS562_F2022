723df5c649982a3752eda340f2668a60a43314f7
Emily <emilysonant@gmail.com>
2022-10-26 12:39:23 -0400

    approximated y'(x) using the finite difference formula for part 1

--- homeworks/hw3/hw3.cpp
+++ homeworks/hw3/hw3.cpp
@@ -17,5 +17,23 @@ float h = std::stof(h_str);
 std::cout << x << std::endl;
 std::cout << h << std::endl;
 
+//part 1 - finite difference formula
+//approximating y' = [f(x+h)-f(x)]/h for y = sin(x)
+
+float y_prime = cos(x); //actual y' = cos(x)
+float y = sin(x); //f(x)
+float y_new = sin(x+h); //f(x+h)
+
+float y_prime_approx = (y_new - y)/h; //approximation for y'
+
+float percent_error = fabs(((y_prime_approx - y_prime)/y_prime)*100); //percent error between approximation and actual value
+
+// std::cout << "the value of y' is actually:\t" << std::endl;
+
+//returns y' actual, expected, and percent error
+std::cout << y_prime << std::endl;
+std::cout << y_prime_approx << std::endl;
+std::cout << percent_error << std::endl;
+
 return 0;
 }
