a78e6895e94b1ef4ae1fa31f8f3fbcbd6f9aa687
Emily <emilysonant@gmail.com>
2022-12-11 02:11:33 -0500

    startedwriting function for computing the average normals

--- final_project/final_project/src/manip.cpp
+++ final_project/final_project/src/manip.cpp
@@ -1,4 +1,6 @@
 #include "manip.hpp"
+#include <cmath>
+#include <algorithm>
 
 // you will need cmath and algorithm headers.
 
@@ -20,6 +22,27 @@ void compute_avg_normals(const SphCo &points, const Triangles &conn,
   nrms.resize(points.npoints());
 
   // your code
+  for(int i = 0; i <= 146; i++){
+    //declaring the current triangle that is being evaluated in conn
+    double curr_conn = conn[i]; 
+
+    //defining the three connections of the triangle
+    conn1 = curr_conn[0];
+    conn2 = curr_conn[1];
+    conn3 = curr_conn[2];
+
+    //defining the coordinates of each connection
+    pts1 = points[conn1];
+    pts2 = points[conn2];
+    pts3 = points[conn3];
+
+    //finding the vectors between the points
+    dist12 = pow((pts2[0] - pts1[0]), 2) + pow((pts2[1] - pts1[1]), 2) + pow((pts2[2] - pts1[2]), 2);
+    dist12 = sqrt(dist12);
+
+    dist13 = pow((pts3[0] - pts1[0]), 2) + pow((pts3[1] - pts1[1]), 2) + pow((pts3[2] - pts1[2]), 2);
+    dist13 = sqrt(dist13);
+  }
 
   // hint don't forget normalizing the normals
 }
