69cd1c906b7209022cd754d27f15e93fb2db41e6
Emily <emilysonant@gmail.com>
2022-12-16 02:30:41 -0500

    fixed compute_n2e_adj and continued working on sum and average adj normals for  compute_avg_normals

--- final_project/final_project/src/manip.cpp
+++ final_project/final_project/src/manip.cpp
@@ -14,26 +14,11 @@ void compute_n2e_adj(const unsigned n, const Triangles &conn,
   // reserve for each of them with a reasonable upper bound
 
   // your code
-  for(unsigned i = 0; i < conn.ntris(); i++) {
+  for (unsigned i = 0; i < conn.ntris(); i++) {
     std::array<int, 3> triangle = conn[i];
-    for(int j = 0; j <= 2; j++) {
+    for (int j = 0; j <= 2; j++) {
       int node = triangle[j];
-      for(unsigned k = 0; k < conn.ntris(); k++) {
-        std::array<int, 3> triangle2 = conn[k];
-        for(unsigned l = 0; l <=2; l++) {
-          int node2 = triangle2[l];
-          /*if (std::find(adj[node].begin(), adj[node].end(), adj[node].size())); {
-            break;
-          } */
-          if(node2 == node) {
-            if(std::find(adj[node].begin(), adj[node].end(), adj[node].size()) = adj[node].end())){
-              break;
-            }
-            adj[node].push_back(k);
-          }
-          break;
-        }
-      }
+      adj[node].push_back(i);
     }
   }
 }
@@ -46,37 +31,48 @@ void compute_avg_normals(const SphCo &points, const Triangles &conn,
 
   // your code
   for (int i = 0; i <= 146; i++) {
-    
     // defining the coordinates of each connection
     std::vector<std::array<double, 3>> pts = points.to_vector();
 
     std::array<double, 3> pts1 = pts[i];
-    std::array<double, 3> pts2 = pts[i+1];
-    std::array<double, 3> pts3 = pts[i+2];
+    std::array<double, 3> pts2 = pts[i + 1];
+    std::array<double, 3> pts3 = pts[i + 2];
 
     // finding the vector between the points
-    std::vector<double> vec12 = {(pts2[0] - pts1[0]), (pts2[1] - pts1[1]), (pts2[2] - pts1[2])};
-    std::vector<double> vec13 = {(pts3[0] - pts1[0]), (pts3[1] - pts1[1]), (pts3[2] - pts1[2])};
+    std::vector<double> vec12 = {(pts2[0] - pts1[0]), (pts2[1] - pts1[1]),
+                                 (pts2[2] - pts1[2])};
+    std::vector<double> vec13 = {(pts3[0] - pts1[0]), (pts3[1] - pts1[1]),
+                                 (pts3[2] - pts1[2])};
 
     // cross product of vector 12 and 13
-    std::vector<double> crossprod = {(vec12[1]*vec13[2] - vec12[2]*vec13[1]), {vec12[2]*vec13[0] - vec12[0]*vec13[2]}, (vec12[0]*vec13[1] - vec12[1]*vec13[0])};
-  
+    std::vector<double> crossprod = {
+        (vec12[1] * vec13[2] - vec12[2] * vec13[1]),
+        {vec12[2] * vec13[0] - vec12[0] * vec13[2]},
+        (vec12[0] * vec13[1] - vec12[1] * vec13[0])};
+
     // magnitude of the cross product
-    double mag = sqrt((pow(crossprod[0], 2)) + pow(crossprod[1], 2) + pow(crossprod[2], 2));
+    double mag = sqrt((pow(crossprod[0], 2)) + pow(crossprod[1], 2) +pow(crossprod[2], 2));
 
     // normalize vectors
-    std::vector<double> norm = {(crossprod[0]/mag), (crossprod[1]/mag), (crossprod[2]/mag)};
+    std::vector<double> norm = {(crossprod[0] / mag), (crossprod[1] / mag), (crossprod[2] / mag)};
+    double normmag = sqrt((pow(norm[0], 2) + (pow(norm[1], 2) + (pow(norm[2], 2)))));
+    norm = {(norm[0]/normmag), (norm[1]/normmag), (norm[2]/normmag)};
   }
 
-  /*for(int v = 0; v < sizeof(n2e_adj); v++) {
-    int k = n2e_adj[v]; 
-    for(int w = 0; w < k; w++) {
-      int z += k[w];
+  //sum over adjacent normals and average them
+  int z = 0;
+  for (unsigned v = 0; v < sizeof(n2e_adj); v++) {
+    std::vector<int> adj_nodes = n2e_adj[v];
+    int k = sizeof(adj_nodes);
+    for (unsigned w = 0; w < sizeof(k); w++) {
+      z += adj_nodes[w];
     }
-    int z = z/k;
-  }*/
+    z = z/k;
+    
+    //normalize vector
+  }
 
-  // hint don't forget normalizing the normals 
+  // hint don't forget normalizing the normals
 }
 
 void compute_errors(const SphCo &exact, const SphCo &num,
